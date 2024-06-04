806fbb0195dceb66b52e4a39a97708173f1cbff5
James SJ Park <ai.jamespark@gmail.com>
2024-06-04 22:14:23 +0900

    240604@home

--- incorrect_assignment_detection/GCCAD/build_graph_v1.py
+++ incorrect_assignment_detection/GCCAD/build_graph_v1.py
@@ -206,6 +206,7 @@ def getdata(orcid):
 
     return (data,edge_label,orcid,all_pappers_id)
 
+'''
 def build_dataset(path):
     
     keys_list = list(author_names.keys())
@@ -216,7 +217,21 @@ def build_dataset(path):
     with open(path, "wb") as f:
         pk.dump(results, f)
     print('finish')
+'''
+
+def build_dataset(path):
+    keys_list = list(author_names.keys())
     
+    ctx = mp.get_context('spawn')  # Use 'spawn' method to avoid issues with 'fork'
+    with ctx.Pool(processes=10) as pool:
+        results = pool.map(getdata, keys_list)
+    
+    with open(path, "wb") as f:
+        pk.dump(results, f)
+    
+    print('finish')
+
+
 def norm(data):
     """
     normalize venue, name and org, for build cleaned graph
@@ -277,20 +292,30 @@ def norm(data):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--author_dir', type=str, default='./IND-WhoIsWho/train_author.json')
-    parser.add_argument('--pub_dir', type=str, default='./IND-WhoIsWho/pid_to_info_all.json')
-    parser.add_argument('--save_dir', type=str, default='./IND-WhoIsWho/train.pkl')
-    parser.add_argument('--embeddings_dir', type=str, default='./IND-WhoIsWho/roberta_embeddings.pkl')
+    parser.add_argument('--author_dir', type=str, default='dataset/train_author.json')
+    parser.add_argument('--pub_dir', type=str, default='dataset/pid_to_info_all.json')
+    parser.add_argument('--save_dir', type=str, default='dataset/train.pkl')
+    parser.add_argument('--embeddings_dir', type=str, default='dataset/roberta_embeddings.pkl')
     args = parser.parse_args()  
     with open(args.pub_dir, "r", encoding = "utf-8") as f:
         papers_info = js.load(f)
 
     # clean pub, if needed
+    '''
     with mp.Pool(processes=10) as pool:
         results = pool.map(norm,[value for _,value  in papers_info.items()])
     papers_info = {k:v for k,v in zip(papers_info.keys(),results)}
     print('done clean pubs')
+    '''
     
+    ctx = mp.get_context('spawn')  # Use 'spawn' method to avoid issues with 'fork'
+    with ctx.Pool(processes=10) as pool:
+        results = pool.map(norm, [value for _, value in papers_info.items()])
+    
+    papers_info = {k: v for k, v in zip(papers_info.keys(), results)}    
+    print('done clean pubs')
+
+
     with open(args.embeddings_dir, "rb") as f:
         dic_paper_embedding = pk.load(f)
     print('done loading embeddings')
